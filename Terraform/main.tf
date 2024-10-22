b24ab12ccf3d99cdf691104329c4157d43bfd41e
Dhanashree <dhanashreekbanshinge@gmail.com>
2024-10-22 15:46:19 +0530

    updated files

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "var.access_key"
-  secret_key = "var.secret_key"
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 resource "aws_vpc" "test" {
@@ -16,7 +16,7 @@ resource "aws_subnet" "main1" {
   vpc_id     = aws_vpc.test.id
   cidr_block = "10.0.1.0/24"
   availability_zone ="ap-south-1a"
-
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "Main1"
@@ -29,12 +29,11 @@ resource "aws_internet_gateway" "IGW" {
     Name = "IGW"
   }
 }
-
 resource "aws_route_table" "Public_RT" {
     vpc_id = aws_vpc.test.id
 
   route {
-    cidr_block = "0.0.0.0/16"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.IGW.id
 }
 }
@@ -45,26 +44,38 @@ resource "aws_route_table_association" "a" {
 }
 
 resource "aws_instance" "Machine1" {
-  ami           = ami-0e0e417dfa2028266
+  ami           = "ami-0e0e417dfa2028266"
   instance_type = "t2.micro"
-  key_name = "key"
+  key_name = aws_key_pair.key.key_name
   subnet_id = aws_subnet.main1.id
+  vpc_security_group_ids = [aws_security_group.new_sg.id]
 
   tags = {
     Name = "Machine1"
   }
+
+  root_block_device {
+    volume_type = "gp2"
+     volume_size = 8
+  }
+
+  user_data = <<EOF
+   #!/bin/bash
+   sudo yum install httpd -y
+   sudo service httpd start
+  EOF
 }
 
-resource "local_file" "Key" {
+resource "local_file" "key" {
 
   content = tls_private_key.rsa.private_key_pem
   filename = "key"
-  
+
 }
 
 resource "aws_key_pair" "key" {
   key_name = "key"
-  public_key = tls_private_key.rsa.public_key_openssh  
+  public_key = tls_private_key.rsa.public_key_openssh
 
 }
 
@@ -74,18 +85,18 @@ resource "tls_private_key" "rsa" {
 }
 
 resource "aws_security_group" "new_sg" {
-  name        = "new_sg"
+  name        = "new"
   description = "Allow http and ssh inbound traffic and all outbound traffic"
   vpc_id      = aws_vpc.test.id
 
   tags = {
-    Name = "new_sg"
+    Name = "new"
   }
 }
 
 resource "aws_vpc_security_group_ingress_rule" "http" {
   security_group_id = aws_security_group.new_sg.id
-  cidr_ipv6         = "0.0.0.0/0"
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
@@ -93,34 +104,19 @@ resource "aws_vpc_security_group_ingress_rule" "http" {
 
 resource "aws_vpc_security_group_ingress_rule" "ssh" {
   security_group_id = aws_security_group.new_sg.id
-  cidr_ipv6         = "0.0.0.0/0"
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
 }
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic" {
-  security_group_id = aws_security_group.allow_new_sg.id
+  security_group_id = aws_security_group.new_sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" 
+  ip_protocol       = "-1"
 }
 
-resource "aws_ebs_volume" "volume" {
-  availability_zone = "ap-south-1a"
-  size              = 8
 
-  tags = {
-    Name = "volume"
-  }
-}
 
-resource "aws_volume_attachment" "attachment" {
-  device_name = "/dev/xvda"
-  volume_id   = aws_ebs_volume.volume.id
-  instance_id = aws_instance.Machine1.id
-}
 
-user_data = << EOF
-#!/bin/bash
-sudo yum install httpd -y
-sudo service httpd start
-EOF
+
+
