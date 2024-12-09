2b8e70477f155ecad4c60fdbd020bfadd07957a0
Dhanashree <dhanashreekbanshinge@gmail.com>
2024-12-09 19:09:25 +0530

    DB instance added

---
+++ Jenkins/jenkins_advance/ec2_instance/output.tf
@@ -0,0 +1,14 @@
+output "aws_instance_id" {
+
+    value = aws_instance.server.id
+}
+
+output "instance_public_ip" {
+
+    value = aws_instance.server.public_ip
+}
+
+output "security_grp" {
+
+    value = aws_security_group.sg.id
+}
